5cda4a751e8239c5218ffd53fff7f4b03c24907e
amareshotta01 <ottaamaresh.2001@gmail.com>
2023-11-11 20:12:31 +0530

    feat: added login signup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const res = require("express/lib/response");
 const connectDB = require('./config/db')
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,8 @@ app.use(express.json());
 app.get("/", (req,res) => {
     res.status(200).json({message : "Hello NexCare!"});
 })
+
+app.use('/api/auth', authRoutes);
 
 app.listen(port,()=>{
     console.log("Server Listening at", port);
